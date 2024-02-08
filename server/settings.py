849dd479b787f9aa324d034082fdf193d3d93215
beomsun0829 <ghsqjatns@gmail.com>
2024-02-08 10:48:25 +0900

    Enable authentication middleware in settings.py

--- server/settings.py
+++ server/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     # "django.middleware.csrf.CsrfViewMiddleware",
-    # "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
