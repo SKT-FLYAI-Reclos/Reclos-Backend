dd11f9b363a9d524b3762712f937be8edcb802f9
beomsun0829 <ghsqjatns@gmail.com>
2024-02-08 10:27:02 +0900

    Update CorsMiddleware import in settings.py

--- server/settings.py
+++ server/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "cors.headers.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
