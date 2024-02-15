c5aef7d4450c979ec6593fa0c2416cdc2fce9d84
beomsun0829 <ghsqjatns@gmail.com>
2024-02-15 16:22:07 +0900

    Refactor serializers in board app

--- board/serializers.py
+++ board/serializers.py
@@ -2,19 +2,21 @@ from rest_framework import serializers
 from .models import Board, Images, Likes
 from user.serializers import UserSerializer
 
-class BoardSerializer(serializers.ModelSerializer):
-    author = UserSerializer(read_only=True)
-    
-    class Meta:
-        model = Board
-        fields = "__all__"
-
 class ImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Images
-        fields = "__all__"
-
+        fields = ['image']
+        
 class LikesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Likes
+        fields = ['user']
+        
+class BoardSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    images = ImagesSerializer(many=True, read_only=True)
+    Likes = LikesSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Board
         fields = "__all__"
