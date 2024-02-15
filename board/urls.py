fdc72ba39d668feb610e122aad15807a0da08fbc
beomsun0829 <ghsqjatns@gmail.com>
2024-02-15 16:10:33 +0900

    fix url

--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
-from .views import BoardView, DummyBoardView
+from .views import BoardView, DummyBoardView, ToggleLikeView
 
 urlpatterns = [
     path("", BoardView.as_view()),
     path("<int:id>/", BoardView.as_view()),
     path("dummy/", DummyBoardView.as_view()),
-    path("<int:id>/like/", DummyBoardView.as_view()),
+    path("<int:id>/like/", ToggleLikeView.as_view()),
 ]
