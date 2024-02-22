4d288f671eebb913c1898d489f975c036da91d61
beomsun0829 <ghsqjatns@gmail.com>
2024-02-22 10:58:11 +0900

    Update imports in views.py

--- board/views.py
+++ board/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 
-from .models import Board, Images, Likes
+from .models import Board, Image, Like
 from user.models import User
 from .serializers import BoardSerializer
 
